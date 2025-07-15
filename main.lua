dd89cede06ac50cbe38147bfbbf55cf31e04f6c7
1v0ry <thedinglepringle@gmail.com>
2025-07-15 12:29:46 +0100

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,44 @@
+SMODS.Atlas({
+    key = "CustomJokers", 
+    path = "CustomJokers.png", 
+    px = 71,
+    py = 95, 
+    atlas_table = "ASSET_ATLAS"
+}):register()
+
+local NFS = require("nativefs")
+to_big = to_big or function(a) return a end
+
+local function load_jokers_folder()
+    local mod_path = SMODS.current_mod.path
+    local jokers_path = mod_path .. "/jokers"
+    local files = NFS.getDirectoryItemsInfo(jokers_path)
+    for i = 1, #files do
+        local file_name = files[i].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("jokers/" .. file_name))()
+        end
+    end
+end
+
+local function load_rarities_file()
+    local mod_path = SMODS.current_mod.path
+    assert(SMODS.load_file("rarities.lua"))()
+end
+
+load_rarities_file()
+
+load_jokers_folder()
+
+
+local card_set_cost_ref = Card.set_cost
+function Card:set_cost()
+    card_set_cost_ref(self)
+    
+    if next(SMODS.find_card("j_noroomfo_clairvoyant")) then
+        if (self.ability.set == 'Spectral' or (self.ability.set == 'Booster' and self.config.center.kind == 'Spectral')) then self.cost = 0 end
+    end
+    
+    self.sell_cost = math.max(1, math.floor(self.cost / 2)) + (self.ability.extra_value or 0)
+    self.sell_cost_label = self.facing == 'back' and '?' or self.sell_cost
+end
